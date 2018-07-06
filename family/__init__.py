281c52b97a754137fbeb14a7e9f285690448482d
Ealautzenhiser <Ealautzenhiser@gmail.com>
2018-07-06 16:55:21 -0400

    Initial Files

---
+++ family/__init__.py
@@ -0,0 +1,25 @@
+import os
+from flask import Flask, render_template
+
+def create_app():
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite')
+    )
+    
+    app.config.from_pyfile('config.py', silent=True)
+
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    from . import db
+    db.init_app(app)
+
+    @app.route('/')
+    def index():
+        return render_template('index.html')
+
+    return app
---
+++ family/schema.sql
@@ -0,0 +1,47 @@
+DROP TABLE IF EXISTS People;
+CREATE TABLE People (
+person_id INTEGER PRIMARY KEY AUTOINCREMENT,
+f_name TEXT NOT NULL,
+m_name TEXT,
+l_name TEXT NOT NULL,
+gender 
+birth_year INTEGER,
+birth_month INTEGER,
+birth_day INTEGER,
+city TEXT,
+state TEXT,
+mother_id INTEGER,
+father_id INTEGER,
+FOREIGN KEY (mother_id) REFERENCES People (person_id),
+FOREIGN KEY (father_id) REFERENCES People (person_id)
+);
+
+DROP TABLE IF EXISTS Marriages;
+CREATE TABLE Marriages (
+spouse1 INTEGER NOT NULL,
+spouse2 INTEGER NOT NULL,
+start_year INTEGER,
+start_month INTEGER,
+start_day INTEGER,
+end_year INTEGER,
+end_month INTEGER,
+end_day INTEGER,
+PRIMARY KEY (spouse1,spouse2)
+);
+
+DROP TABLE IF EXISTS Memoirs;
+CREATE TABLE Memoirs (
+memoir_id INTEGER PRIMARY KEY AUTOINCREMENT,
+name TEXT NOT NULL,
+author_id INTEGER NOT NULL,
+FOREIGN KEY (author_id) REFERENCES People (person_id)
+);
+
+DROP TABLE IF EXISTS Memoir_tags;
+CREATE TABLE Memoir_tags (
+memoir_id INTEGER NOT NULL,
+person_id INTEGER NOT NULL,
+PRIMARY KEY (memoir_id, person_id),
+FOREIGN KEY (memoir_id) REFERENCES Memoirs (memoir_id),
+FOREIGN KEY (person_id) REFERENCES People (person_id)
+);
